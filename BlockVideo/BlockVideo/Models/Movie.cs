62d50418644b54f7b7472c19666010672dab553a
Josh <joshuawhite1@u.boisestate.edu>
2018-03-15 00:03:28 -0600

    Created First Movie Models, Views and Controllers

---
+++ BlockVideo/BlockVideo/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlockVideo;
+namespace BlockVideo.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
