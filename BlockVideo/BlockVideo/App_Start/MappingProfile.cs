530b749eee80429a5a79fdc49bb4a923f1112f3f
Josh <joshuawhite1@u.boisestate.edu>
2018-03-19 18:20:33 -0600

    Added Automapper functionality to CustomersController (API)

---
+++ BlockVideo/BlockVideo/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using BlockVideo.Dtos;
+using BlockVideo.Models;
+
+namespace BlockVideo.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ BlockVideo/BlockVideo/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BlockVideo.Models;
+
+namespace BlockVideo.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter Customer's Name.")]
+        [StringLength(255)]
+        public String Name { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
