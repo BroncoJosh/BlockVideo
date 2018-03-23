59958a3e5f8e1bb43831bed6210bfdfdff398fe8
Josh <joshuawhite1@u.boisestate.edu>
2018-03-23 16:42:37 -0600

    Added Social Authentication for Facebook. Removed appId and appSecret from commit.

--- BlockVideo/BlockVideo/App_Start/Startup.Auth.cs
+++ BlockVideo/BlockVideo/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace BlockVideo
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+//            app.UseFacebookAuthentication(
+//               appId: "",
+//               appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
